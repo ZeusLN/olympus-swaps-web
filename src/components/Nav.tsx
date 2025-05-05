117d9370a6ff724a05c5bc966168ccfd39d86f9a
Shubham Kumar <shubham@zeusln.com>
2025-05-06 00:28:37 +0530

    Fix Navbar alignment

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,61 @@
 import { A } from "@solidjs/router";
-import { AiOutlineHome } from "solid-icons/ai";
+import { AiFillHome } from "solid-icons/ai";
 
 import logo from "../assets/olympus-z-white.svg";
 
 const Nav = () => {
     return (
-        <div class="nav-container">
-            <A id="logo" href="/" class="nav-left">
-                <img src={logo} alt="Olympus by ZEUS logo" class="logo-small" />
-                <p>Swaps by ZEUS LSP</p>
-            </A>
+        <div
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100%",
+            }}>
+            <div
+                style={{
+                    position: "absolute",
+                    left: "10px",
+                    top: "10px",
+                    display: "flex",
+                    "align-items": "center",
+                }}>
+                <A
+                    href="/"
+                    style={{
+                        "text-decoration": "none",
+                        color: "inherit",
+                        display: "flex",
+                        "align-items": "center",
+                    }}>
+                    <img
+                        src={logo}
+                        height="30"
+                        alt="Swaps by ZEUS LSP"
+                        style={{ "margin-right": "8px" }}
+                    />
+                    <p
+                        class="hide-on-mobile"
+                        style={{ "font-size": "18px", margin: 0 }}>
+                        Swaps by ZEUS LSP
+                    </p>
+                </A>
+            </div>
 
-            <A href="https://olympusln.com" class="nav-right">
-                <AiOutlineHome size={24} color="white" />
-            </A>
+            <div style={{ position: "absolute", right: 0, top: 0 }}>
+                <A
+                    href="https://olympusln.com"
+                    style={{
+                        "text-decoration": "none",
+                        color: "inherit",
+                        padding: "10px",
+                        display: "flex",
+                        "align-items": "center",
+                        "justify-content": "center",
+                    }}>
+                    <AiFillHome size={25} color="white" />
+                </A>
+            </div>
         </div>
     );
 };
